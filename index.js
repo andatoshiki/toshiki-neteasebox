21be6e16337498c70b09cf4e4ae5784fc5ccc3df
andatoshiki <a_toshiki@outlook.com>
2022-04-06 19:38:59 +0800

    init

---
+++ index.js
@@ -0,0 +1,76 @@
+require('dotenv').config();
+const { Octokit } = require('@octokit/rest');
+const { user_record } = require('NeteaseCloudMusicApi');
+
+const {
+  GIST_ID: gistId,
+  GH_TOKEN: githubToken,
+  USER_ID: userId,
+  USER_TOKEN: userToken,
+} = process.env;
+
+(async () => {
+  /**
+   * First, get user record
+   */
+
+  const record = await user_record({
+    cookie: `MUSIC_U=${userToken}`,
+    uid: userId,
+    type: 1, // last week
+  }).catch(error => console.error(`Unable to get user record \n${error}`));
+
+  /**
+   * Second, get week play data and parse into song/plays diagram
+   */
+
+  let totalPlayCount = 0;
+  const { weekData } = record.body;
+  weekData.forEach(data => {
+    totalPlayCount += data.playCount;
+  });
+
+  const icon = ['🥇', '🥈', '🥉', '', '']
+
+  const lines = weekData.slice(0, 5).reduce((prev, cur, index) => {
+    const playCount = cur.playCount;
+    const artists = cur.song.ar.map(a => a.name);
+    let name = `${cur.song.name} - ${artists.join('/')}`;
+
+    const line = [
+      icon[index].padEnd(2),
+      name,
+      ' · ',
+      `${playCount}`,
+      'plays',
+    ];
+
+    return [...prev, line.join(' ')];
+  }, []);
+
+  /**
+   * Finally, write into gist
+   */
+
+  try {
+    const octokit = new Octokit({
+      auth: `token ${githubToken}`,
+    });
+    const gist = await octokit.gists.get({
+      gist_id: gistId,
+    });
+
+    const filename = Object.keys(gist.data.files)[0];
+    await octokit.gists.update({
+      gist_id: gistId,
+      files: {
+        [filename]: {
+          filename: `🎵 My last week in music`,
+          content: lines.join('\n'),
+        },
+      },
+    });
+  } catch (error) {
+    console.error(`Unable to update gist\n${error}`);
+  }
+})();
